c6eb37bea6dcd606a7fb3a2102de4b8c44743e1e
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-28 21:03:04 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,10 +3,12 @@ def square_array(array)
   new_array = []
 
 
-  array.each do |element|
-  new_array << (element ** 2) 
+  array.each do { |element|
+  new_array << element ** 2) 
+    
+  }
 
-#arr = []
+
  # array.each { |i| arr << i ** 2 }
 #  arr
 
